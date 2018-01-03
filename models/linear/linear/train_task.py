40e4b7acad1b1e15d0ecb61e9742b37807accd88
andrefsp <andrefsp@gmail.com>
2018-01-03 17:26:27 +0000

    Add documentation.

--- models/linear/linear/train_task.py
+++ models/linear/linear/train_task.py
@@ -6,7 +6,17 @@ from tensorflow.contrib.training.python.training import hparam
 from linear import LinearModel
 
 
+# Google Cloud ML Engine requires run_experiment.
+# ``run_experiment`` is the entry point for ML engine jobs.
+# As this is a train job than we instantiate the LinearModel class and we
+# train the model from a file passed through hparams and we specify the
+# export path to product the model files. (*.pb)
+
+
 def run_experiment(hparams):
+    '''
+    Google ML Engine entry point for training job.
+    '''
     linear_model = LinearModel(hparams.export_path)
     linear_model.train_from_file(hparams.train_file)
 
@@ -26,13 +36,13 @@ if __name__ == '__main__':
         default="./export/",
         required=True
     )
+    # passed only in ML engine.
     parser.add_argument(
         '--job-dir',
         help='GCS or local paths to training data',
         default="./job/",
         required=False
     )
-
     parser.add_argument(
         '--verbosity',
         choices=[
